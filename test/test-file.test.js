3cc2f59a519b6af26650e611c072d3b19044641b
Bouya-Sensei <37253145+BouyaSensei@users.noreply.github.com>
2024-06-24 17:44:50 +0200

    Update test-file.test.js

--- test/test-file.test.js
+++ test/test-file.test.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
-const search = require('../search.js');
+import search from "./search.js";
+import {expect} from 'chai';
 
 describe('functionnalité de recherche', function() {
     it('doit retourner un resulter selon les elements entrer', function() {
         const results = search('Paris', 'Lyon', '2024-06-24');
-        assert(Array.isArray(results), 'Resultat doit etre un tableau');
-        assert(results.length > 0, 'résultat ne doit pas etre un tableau vide');
+        expect(Array.isArray(results), 'Resultat doit etre un tableau');
+        expect(results.length > 0, 'résultat ne doit pas etre un tableau vide');
     });
 
 });
